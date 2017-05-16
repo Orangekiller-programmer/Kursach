cbaf0e9dc37fd99b65a3c3afba046f2f43da2578
Orangekiller-programmer <makssablinmix@mail.ru>
2017-05-16 21:50:07 +0300

    Add files via upload

---
+++ Kursach/Kurs/Rabotaushiy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+
+    class Rabotaushiy : Vipusknik
+    {
+        public Rabotaushiy(string number,string group, string kodspec, string rabota)
+            : base(number, group, kodspec, rabota)
+            { }
+
+
+
+
+        public string Group
+        {
+            get
+            {
+
+                return group;
+
+            }
+
+            set
+            {
+                group = value;
+            }
+
+
+        }
+
+        }
+
+
+
+
+    }
+
+
